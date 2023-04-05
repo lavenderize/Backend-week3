7cd73a9c4d13cb234d6279318c2632a11ec87009
haneunseon <kodrdja25@gmail.com>
2023-04-05 20:46:06 +0900

    4th

--- ans/4.py
+++ ans/4.py
@@ -1,7 +1,14 @@
 # 4. 
-
 animals = ('lion', 'tiger', 'penguin', 'dog', 'cat')
 
+newanimals = list(animals)
+answer = []
 
+for i in newanimals :
+    if 'e' not in i:
+        answer.append(i)
+        
+
+print(answer)
 
 
