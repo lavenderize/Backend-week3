48ad8d28fd9c78e7a04cf47ac3c8b1863cd4044a
kseysh <kseysh123@naver.com>
2023-04-05 20:58:53 +0900

    add 7.py

--- ans/7.py
+++ ans/7.py
@@ -1,11 +1,19 @@
 # 7.
+from movies import *
+import random
 
-
-
-# class RandomCinema():
+class RandomMovie(Movie,Cinema):
     
+    
+    def __init__(self):
+        super().__init__()
 
+    def RandomCinema(self):
+        cinema = Cinema()
+        movie = input()
+        ranNum = random.randrange(1,len(cinema.cinema_list))
+        print(movie,cinema.cinema_list[ranNum])
 
-
-
+rand = RandomMovie()
+rand.RandomCinema()
 
