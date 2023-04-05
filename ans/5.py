a3aa2edd4cfde9a442fdcf3ea96af9000cf46459
lavenderize <liexquisit@gmail.com>
2023-04-05 20:47:14 +0900

    05

--- ans/5.py
+++ ans/5.py
@@ -1,10 +1,8 @@
 # 5.
 
-# def print_max():
-    
-    
+def print_max():
+    a, b, c, d, e = map(int, input("정수 5개 입력(띄어쓰기로 구분):").split())
+    a_list = [a, b, c, d, e]
+    print(max(a_list))
 
-
-
-
-# print_max()
+print_max()
