7e6f850b25e9b8f610751147ad786767e635c4ec
soozzang <soozzang@inha.edu>
2023-04-05 19:51:12 +0900

    2번

--- ans/2.py
+++ ans/2.py
@@ -1,4 +1,13 @@
 # 2. 
+# 2. 
+for i in range (4):
+    name=input("이름을 입력해주세요:")
+    department=input("학과를 입력해주세요:")
+    number=input("학번을 입력해주세요:")
+    ans= f'{name}({number})님의 학과는 {department}.'
+    print(ans)
+
+    
 
 
     
